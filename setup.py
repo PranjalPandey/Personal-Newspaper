c0bef125eccb19d46752f57487f454af86aa8204
PranjalPandey <pranjalpandeyshine@gmail.com>
2025-04-08 22:28:22 +0530

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="morning",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[line.strip() for line in open("requirements.txt")
+                     if not line.startswith("#") and line.strip()],
+)
